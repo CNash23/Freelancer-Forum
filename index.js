55cbcd7a0015133199846d6c3b24e87eaf7313bc
Clea Carty <clea.nash@yahoo.com>
2023-05-13 23:54:41 -0400

    added html, styling and functions to javascript

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ const users = [
 
 function main() {
 
+
+
+const root = document.getElementById("root") //1.grab the "root" element
+
+const h1 = document.createElement("h1") //create an h1 element
+
+h1.innerHTML = "FREELANCERS"//give it a text of "FREELANCERS"
+
+root.append(h1) //append it to the HTML document
+
+const unordList = document.createElement("ul")//create an unordered list element (unordList)
+
+ for (let i = 0; i < users.length; i++){   //loop through the users array creating li elements (list)
+  let list = document.createElement("li") 
+  list.innerText = users[i].name
+  
+  unordList.appendChild(list) //Append li elements to ul parent(unordList).
+    
+ }  
+ document.querySelector("#root").appendChild(unordList) //Append ul element to your HTML document.
+ 
 }
 
-//call the main function
-main();
+
+main(); //call the main function
